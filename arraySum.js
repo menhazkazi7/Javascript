ba33ed304da2fb053c856620c3da3de99cd5ad19
menhazkazi7 <96825908+menhazkazi7@users.noreply.github.com>
2022-05-26 21:50:52 +0600

    Add files via upload

---
+++ arraySum.js
@@ -0,0 +1,10 @@
+var numbers = [45,78,12,3,54,65];
+var sum = 0;
+
+
+for( i = 0; i < numbers.length; i++){
+    var element  = numbers[i];
+    sum = sum + element;
+
+}
+console.log("Total number is : ", sum);
