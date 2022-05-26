636795b4758994e584bf666a1dfa0f264b0121e2
menhazkazi7 <96825908+menhazkazi7@users.noreply.github.com>
2022-05-26 21:53:18 +0600

    Add files via upload

---
+++ wordcount.js
@@ -0,0 +1,14 @@
+var speech = "I am kazi Menhaz."
+
+var count = 0;
+for(var i=0; i <speech.length; i ++){
+    var char = speech[i];
+    
+    if( char ==" "){
+        count ++;
+
+    }
+    
+}
+count++;
+console.log(count);
